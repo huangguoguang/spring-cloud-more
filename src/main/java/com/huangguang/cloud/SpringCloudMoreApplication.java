3a06a310b45a25842045b3ef02fe03d184c1e257
huangguang <huangguang@anchol.com>
2018-01-18 16:45:12 +0800

    eureka集群使用

---
+++ src/main/java/com/huangguang/cloud/SpringCloudMoreApplication.java
@@ -0,0 +1,14 @@
+package com.huangguang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudMoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMoreApplication.class, args);
+	}
+}
